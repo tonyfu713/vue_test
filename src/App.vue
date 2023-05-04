686db0de3d8f498cc0726224310a83e51f3430bb
TONYFU\WIN10 <tonyfu13@gmail.com>
2023-05-04 10:34:19 +0800

    222

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,68 @@
-<script setup>
-import HeaderView from "./components/Header.vue"
+<script>
+// import apple from ""
+export default {
+components:{
+
+},
+  data() {
+    return{
+      A:1,
+      temp:666
+    
+
+    }
+  },
+  methods: {
+    changeAValue(){
+      this.A = this.temp;
+    }
+    
+  },
+  mounted(){
+
+  }
+}
+
 </script>
 
 <template>
-    <div class="container">
-        <HeaderView />
-    </div>
+  <h1>123123</h1>
+  <div class="text-area">
+    <h2>V-model</h2>
+    <h2>V-on</h2>
+    <h3>A:{{ A }}</h3>
+    <p>temp:{{ temp }}</p>
+    <input type="number" v-model="temp">
+    <button type="button" v-on:click="changeAValue">update</button>
+
+  </div>
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+}
 
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
